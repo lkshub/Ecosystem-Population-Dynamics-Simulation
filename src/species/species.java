f9f3597266b6a5d329eb1b578f146b6fc0d3bbc9
Tong JIN <tongjin1992@gmnail.com>
2016-04-04 11:58:31 -0400

    Tong modified on 4/4 Add main class for the software

--- src/species/species.java
+++ src/species/species.java
@@ -1,5 +1,31 @@
 package species;
 
-class species {
+import java.util.List;
 
+class species {
+	private int population;
+	private List<Integer> predator;
+	private List<Integer> prey;
+	
+	public void setPopulation(int population){
+		this.population = population;
+	}
+	
+	public int getPopulation(){
+		return population;
+	}
+	
+	public void setPredator(List<Integer> predator){
+		this.predator = predator;
+	}
+	
+	public List<Integer> getPredator(){
+		return predator;
+	}
+	
+	public void setPrey(List<Integer> prey){
+		this.prey = prey;
+	}
+	
+	
 }
