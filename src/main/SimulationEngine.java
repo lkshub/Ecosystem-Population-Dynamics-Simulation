3dfea04b26aa63e14d00ea3cc814880482a3ff0e
Lu, Kai <klu65@gatech.edu>
2016-04-13 13:56:15 -0400

    4-13 meeting

--- src/main/SimulationEngine.java
+++ src/main/SimulationEngine.java
@@ -11,33 +11,58 @@ public class SimulationEngine {
 	
 	/**
 	 * This function is used for the initializing the system at first.
+	 * Input:K,n,L,c
+	 * Output:alpha,S,f
 	 */
 	//Yuzhi
-	public static void initialize(int numSpecies, List<Species> speciesList){
-		for (int i=0; i<numSpecies; i++) {
-			Species s = new Species();
-			s.setPopulation((int)Function.GaussianRNG(100, 20));
-			speciesList.add(s);
-		}
+	public static void initialize(int K,int n,int L,double c, List<List<Double>> alpha, List<List<Double>> S, List<List<Double>> f, List<List<Boolean>> attr, List<Integer> N){
+
 	}
 	
 	/**
 	 * This function is used for updating the population of species for each time step.
 	 */
 	//Qiang
-	public static void update(List<Species> speciesList, List<List<Integer>> speciesRelation){
+	public static void update(double b,List<Species> f, List<List<Integer>> speciesRelation){
 		
 		
 		
-		environmentUpdate(speciesList);
+
 	}
-	
+	/**
+	 * Input:f,b,alpha,N,S
+	 * Output:f,g
+	 * @param f
+	 * @param b
+	 * @param g
+	 * @param alpha
+	 * @param N
+	 * @param S
+	 * @return
+	 */
+	private static double updateParameters( List<List<Double>> f,double b, List<List<Double>> g, List<List<Double>> alpha,List<Integer> N, List<List<Double>> S){
+		double error=0;
+		
+		
+		return error;
+		
+	}
+	/**
+	 * Input:N,deltaT,g
+	 * Output:N
+	 * @param N
+	 * @param deltaT
+	 * @param g
+	 */
+	private static void updatePopulation(List<Integer> N, double deltaT, List<List<Double>> g){
+		
+	}
 	/**
 	 * This function is used for update the environment such as whether new species comes in 
 	 * and whether the species relationship changes.
 	 */
 	//Tong
-	private static void environmentUpdate(List<Species> speciesList){
+	private static void generateNewSpecies( List<List<Double>> S, List<List<Double>> alpha, List<List<Double>> f, List<Integer> N,List<List<Boolean>> attr){
 		
 	}
 	
