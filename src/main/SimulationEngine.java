8413b1ab3bcee896462cf31b9441232e344328d0
Lu, Kai <klu65@gatech.edu>
2016-04-13 20:22:35 -0400

    finish the updateParameters method of class simulatinEngine

--- src/main/SimulationEngine.java
+++ src/main/SimulationEngine.java
@@ -1,6 +1,7 @@
 package main;
 
 import java.util.List;
+import java.lang.Math;
 
 import functions.*;
 import variable.Species;
@@ -42,7 +43,28 @@ public class SimulationEngine {
 	 */
 	private static double updateParameters( List<List<Double>> f,double b, List<List<Double>> g, List<List<Double>> alpha,List<Integer> N, List<List<Double>> S){
 		double error=0;
-		
+		for(int i=0;i<g.size();i++){
+			for(int j=0;j<g.get(i).size();j++){
+				double sum=0;
+				for(int k=0;k<g.size();k++){
+					sum = sum + alpha.get(k).get(i)*S.get(k).get(j)*f.get(k).get(j)*N.get(k);		
+				}
+				g.get(i).set(i,S.get(i).get(j)*f.get(i).get(j)*N.get(j)/(b*N.get(j)+sum));
+			}
+		}
+		for(int i=0;i<g.size();i++){
+			for(int j=0;j<g.get(i).size();j++){
+				double sum=0;
+				double oldValue=f.get(i).get(j);
+				for(int k=0;k<g.size();k++){
+					sum = sum + g.get(i).get(k);		
+				}
+				double newValue=g.get(i).get(j)/sum;
+				f.get(i).set(i,newValue);
+				error=Math.max(error,Math.abs((newValue-oldValue)/oldValue));
+			}
+		}
+			
 		
 		return error;
 		
